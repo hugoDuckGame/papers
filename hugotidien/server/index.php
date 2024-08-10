1e8d26c53d784f9108b9d7d9981f4759ca9275cc
hugoDuckGame <pingouinafk255@gmail.com>
2024-08-10 21:43:42 +0200

    created base folders and index pages

---
+++ hugotidien/server/index.php
@@ -0,0 +1,101 @@
+<html>
+    <head>
+        <title>Hugotidien</title>
+        <link rel="stylesheet" href="hugotidien.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    </head>
+    <body>
+        <div class="container-fluid">
+            <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>                        
+            </button>
+            <a class="navbar-brand" href="#"></a>
+            </div>
+            <div class="collapse navbar-collapse" id="myNavbar">
+            <ul class="nav navbar-nav">
+                <li class="active"><a href="#"></a></li>
+                <li><a href="#"></a></li>
+            </ul>
+            <form class="navbar-form navbar-right" role="search">
+                <div class="form-group input-group">
+                <input type="text" class="form-control">
+                <span class="input-group-btn">
+                    <button class="btn btn-default" type="button">
+                    <span class="glyphicon glyphicon-search"></span>
+                    </button>
+                </span>        
+                </div>
+            </form>
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="#"><span class="glyphicon glyphicon-user"></span></a></li>
+            </ul>
+            </div>
+        </div>
+        <div class="container text-center">    
+        <div class="row">
+            <div class="col-sm-3 well">
+            <div class="well">
+                <p><a href="#"></a></p>
+                <img src="" class="img-circle" height="65" width="65" alt="">
+            </div>
+            <div class="well">
+                <p><a href="#"></a></p>
+                <p>
+                <span class="label label-default"></span>
+                <span class="label label-primary"></span>
+                <span class="label label-success"></span>
+                <span class="label label-info"></span>
+                <span class="label label-warning"></span>
+                <span class="label label-danger"></span>
+                </p>
+            </div>
+            <div class="alert alert-success fade in">
+                <a href="#" class="close" data-dismiss="alert" aria-label="close">×</a>
+                <p><strong></strong></p>
+            </div>
+            <p><a href="#"></a></p>
+            <p><a href="#"></a></p>
+            <p><a href="#"></a></p>
+            </div>
+            <div class="col-sm-7">
+            <div class="row">
+                <div class="col-sm-12">
+                <div class="panel panel-default text-left">
+                    <div class="panel-body">
+                    <p contenteditable="true"></p>
+                    <button type="button" class="btn btn-default btn-sm">
+                        <span class="glyphicon glyphicon-thumbs-up"></span>
+                    </button>     
+                    </div>
+                </div>
+                </div>
+            </div>   
+            </div>
+            <div class="col-sm-2 well">
+            <div class="thumbnail">
+                <p></p>
+                <img src="" alt="" width="400" height="300">
+                <p><strong></strong></p>
+                <p></p>
+                <button class="btn btn-primary"></button>
+            </div>      
+            <div class="well">
+                <p></p>
+            </div>
+            <div class="well">
+                <p></p>
+            </div>
+            </div>
+        </div>
+    </div>
+    <footer class="container-fluid text-center">
+    <p></p>
+    </footer>
+
+    </body>
+</html>
